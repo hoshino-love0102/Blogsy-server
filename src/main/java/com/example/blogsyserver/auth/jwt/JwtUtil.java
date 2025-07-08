0cf4c83a376b4154d8be8dcbc8fa3ed6315b9045
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-08 09:37:04 +0900

    JWT 설정 추가

---
+++ src/main/java/com/example/blogsyserver/auth/jwt/JwtUtil.java
@@ -0,0 +1,18 @@
+package com.example.blogsyserver.auth.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+}
