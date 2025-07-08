686c5ab9817dba04b19cdf039dd3b410ab6dbbd4
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-08 09:41:51 +0900

    feat(jwt): JwtAuthenticationFilter 클래스 생성 및 기본 뼈대 구현

---
+++ src/main/java/com/example/blogsyserver/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.example.blogsyserver.auth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
